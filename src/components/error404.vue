99c63d4d3d6816f9fb517c16662181c2b9187865
medukr <andrii.demydyuk@gmail.com>
2019-05-27 17:16:32 +0300

    + Переход назад со страницы ошибки

--- src/components/error404.vue
+++ src/components/error404.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="error">
         <div class="error__content">
-            <h2>500</h2>
-            <h3>Something went wrong!</h3>
+            <h2>404</h2>
+            <h3>Что-то пошло не так!</h3>
             <p>There was a problem on our end. Please try again later.</p>
-            <button type="button" class="btn btn-accent btn-pill">&larr; Go Back</button>
+            <button type="button" class="btn btn-accent btn-pill" @click="onClick">&larr; Назад</button>
         </div>
         <!-- / .error_content -->
     </div>
@@ -12,7 +12,11 @@
 
 <script>
     export default {
-
+        methods: {
+            onClick(){
+                this.$router.back();
+            }
+        }
     }
 </script>
 
